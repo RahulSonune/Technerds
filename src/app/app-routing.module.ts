024fe49e7c93a85ba5104e87522bd68dfb2e6112
ashtamarajbinde <amrajbinde@gmail.com>
2023-03-10 13:49:59 +0545

    department

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddDepartmentComponent } from './add-department/add-department.component';
+import { EditDepartmentComponent } from './edit-department/edit-department.component';
+import { ListDepartmentComponent } from './list-department/list-department.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'add-department',
+    component:AddDepartmentComponent
+  },
+  {
+    path:"edit-department",
+    component:EditDepartmentComponent
+  },
+  {
+    path:"list-department",
+    component:ListDepartmentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AddDepartmentComponent } from './add-department/add-department.component';
+import { EditDepartmentComponent } from './edit-department/edit-department.component';
+import { ListDepartmentComponent } from './list-department/list-department.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddDepartmentComponent,
+    EditDepartmentComponent,
+    ListDepartmentComponent
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
