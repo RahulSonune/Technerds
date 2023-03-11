90260b32a9aedae4211bc9aa8ea3960da90d8a5b
ashtamarajbinde <amrajbinde@gmail.com>
2023-03-11 09:54:02 +0545

    me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
 import { HeaderComponent } from './header/header.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
