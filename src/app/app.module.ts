a2ae4da6eeee172eea4bb39fe9f8b887df5d49ad
AratiShahare <shaharearati0@gmail.com>
2023-03-14 13:43:22 +0530

    added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-<<<<<<< HEAD
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-=======
+
 import { SubjectAddComponent } from './subject/subject-add/subject-add.component';
 import { SubjectEditComponent } from './subject/subject-edit/subject-edit.component';
 import { SubjectListComponent } from './subject/subject-list/subject-list.component';
->>>>>>> a8ddf1e316b98513f7da710ad30bd7ed84052c34
 
 
 
@@ -27,24 +24,24 @@ import { SubjectListComponent } from './subject/subject-list/subject-list.compon
     HeaderComponent,
     SidenavComponent,
     DashboardComponent,
-<<<<<<< HEAD
-=======
+
+
     SubjectAddComponent,
     SubjectListComponent,
     SubjectEditComponent
->>>>>>> a8ddf1e316b98513f7da710ad30bd7ed84052c34
+
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
-<<<<<<< HEAD
-    BrowserAnimationsModule 
-=======
-    BrowserAnimationsModule,
+
+    BrowserAnimationsModule ,
+
+    
     MatSlideToggleModule
->>>>>>> a8ddf1e316b98513f7da710ad30bd7ed84052c34
+
       ],
   providers: [],
   bootstrap: [AppComponent]
